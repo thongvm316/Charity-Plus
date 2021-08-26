9815451e5f1d6cca8c76d3ab2e58edfdfa71d394
BM James <thongvm@brickmate.kr>
2021-08-26 21:22:14 +0700

    2021.08.26

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 import AppBar from '@material-ui/core/AppBar'
 import Box from '@material-ui/core/Box'
-import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
+import Container from 'components/CustommContainer/CustomContainer'
+import IconButton from '@material-ui/core/IconButton'
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined'
+import PhoneCallbackOutlinedIcon from '@material-ui/icons/PhoneCallbackOutlined'
 
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
@@ -10,31 +14,59 @@ import RestoreIcon from '@material-ui/icons/Restore'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 
-import 'assets/scss/MainLayout/header.scss'
+import 'assets/scss/layout/main-layout.scss'
 
 const MainLayout = () => {
   const [value, setValue] = React.useState(0)
   return (
     <>
-      <AppBar className='header' position='static'>
-        <Container fixed maxWidth='xl'>
-          <Box className='header__contact'></Box>
+      <Box className='header'>
+        <Box className='header__contact'>
+          <Container className='header__container'>
+            <p>
+              <IconButton>
+                <MailOutlineOutlinedIcon className='header__icon' />
+              </IconButton>
+              info@charityplus
+            </p>
+            <Divider
+              orientation='vertical'
+              className='header__divide'
+              flexItem
+            />
+            <p>
+              <IconButton>
+                <PhoneCallbackOutlinedIcon className='header__icon' />
+              </IconButton>
+              +91 8857 665 660
+            </p>
+          </Container>
+        </Box>
 
-          <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-              setValue(newValue)
-            }}
-            showLabels
-          >
-            <BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
-            <BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
-            <BottomNavigationAction label='Nearby' icon={<LocationOnIcon />} />
-          </BottomNavigation>
-        </Container>
-      </AppBar>
+        <AppBar className='header__menu' position='static'>
+          <Container>
+            <BottomNavigation
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue)
+              }}
+              showLabels
+            >
+              <BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
+              <BottomNavigationAction
+                label='Favorites'
+                icon={<FavoriteIcon />}
+              />
+              <BottomNavigationAction
+                label='Nearby'
+                icon={<LocationOnIcon />}
+              />
+            </BottomNavigation>
+          </Container>
+        </AppBar>
+      </Box>
 
-      {/* <footer>Footer</footer> */}
+      <Box className='footer'></Box>
     </>
   )
 }
